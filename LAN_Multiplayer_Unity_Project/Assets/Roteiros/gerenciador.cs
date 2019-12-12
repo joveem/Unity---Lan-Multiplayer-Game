7a747815186bc145bc87df879452dce02b1696b9
joveem <umjoveem@gmail.com>
2019-12-12 16:23:22 -0300

    #upload001
    -repository start

---
+++ LAN_Multiplayer_Unity_Project/Assets/Roteiros/gerenciador.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gerenciador : MonoBehaviour
+{
+    static public gerenciador instancia;
+    public movimento mov_jog;
+    public bool pausado;
+    public GameObject menu_pausa;
+    // Start is called before the first frame update
+
+    void Awake(){
+        instancia = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            alternarTravamentoDeMouse();
+        }
+    }
+
+    
+
+    public void alternarTravamentoDeMouse()
+    {
+        if (!pausado)
+        {
+            menu_pausa.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            pausado = !pausado;
+            mov_jog.pausado = !mov_jog.pausado;
+        }
+        else
+        {
+            menu_pausa.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            pausado = !pausado;
+            mov_jog.pausado = !mov_jog.pausado;
+        }
+    }
+}
